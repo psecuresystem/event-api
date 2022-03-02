70df59549674a342e8f488e5364265e1ccde435c
psecuresystem <92157374+psecuresystem@users.noreply.github.com>
2022-03-02 12:10:50 +0100

    Delete Event Api directory

--- Event Api/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('mongodb://localhost:27017/Events')
-    .then(() => {
-        console.log('Successfully connected to mongodb')
-    }).catch(err => {
-        console.log(err)
-    })
--- Event Api/utils/generateToken.js
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken'
-export default function GenerateToken({_id, email}){
-    return jwt.sign({_id,email}, 'My name is vision onyeaku')
-}
